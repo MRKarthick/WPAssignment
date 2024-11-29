aa62148c7fbf562f99284a61fa461beeef31130c
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-11-29 22:45:25 +1300

    MVVM Structure.

---
+++ WPAssignment/WPAssignment/Cards/View/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  WPAssignment
+//
+//  Created by EdgeCaseDesigns on 29/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Item List")
+    }
+}
+
+#Preview {
+    ContentView()
+}
