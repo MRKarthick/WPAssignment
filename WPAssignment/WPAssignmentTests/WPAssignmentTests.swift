7f9fb293266c382ed59ed4498f0886ba0969bc47
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-11-29 22:19:13 +1300

    Base Project

---
+++ WPAssignment/WPAssignmentTests/WPAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  WPAssignmentTests.swift
+//  WPAssignmentTests
+//
+//  Created by EdgeCaseDesigns on 29/11/24.
+//
+
+import Testing
+@testable import WPAssignment
+
+struct WPAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
