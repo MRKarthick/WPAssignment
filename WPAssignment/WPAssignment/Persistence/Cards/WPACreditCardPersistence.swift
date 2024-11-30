5dffd66bab0e99e96e3b51f609ec5f467fb6dda8
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-11-30 17:45:15 +1300

    CreditCardPersistence

--- WPAssignment/WPAssignment/Persistence/Cards/WPACreditCardPersistence.swift
+++ WPAssignment/WPAssignment/Persistence/Cards/WPACreditCardPersistence.swift
@@ -6,7 +6,37 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUICore
+import _SwiftData_SwiftUI
 
 class WPACreditCardPersistence {
+    private let modelContainer: ModelContainer
+
+    @Query private var creditCards: [WPACreditCardEntity]
     
+    static let shared = WPACreditCardPersistence()
+    
+    init() {
+        do {
+            self.modelContainer = try ModelContainer(for: WPACreditCardEntity.self)
+        } catch {
+            fatalError("Failed to initialize ModelContainer: \(error)")
+        }
+    }
+    
+    @MainActor func saveCard(_ creditCard: WPACreditCardEntity) {
+        let context = modelContainer.mainContext
+        context.insert(creditCard)
+        
+        do {
+            try context.save()
+        } catch {
+            debugPrint("Failed to save credit card: \(error)")
+        }
+    }
+    
+    func retrieveCreditCards() -> [WPACreditCardEntity] {
+        return creditCards
+    }
 }
