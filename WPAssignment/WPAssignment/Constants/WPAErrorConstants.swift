7f72d918bf93a873006eac298937327f71c28dc0
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-12-02 08:27:27 +1300

    Test case correction

--- WPAssignment/WPAssignment/Constants/WPAErrorConstants.swift
+++ WPAssignment/WPAssignment/Constants/WPAErrorConstants.swift
@@ -10,5 +10,5 @@ import Foundation
 struct WPAErrorConstants {
     static let kGenericErrorTitle: String = "Please try again in sometime"
     static let kFailedCreditCardsErrorTitle: String = "Failed to fetch cards"
-    static let kFailedBookmarksErrorTitle: String = "Failed to fetch cards"
+    static let kFailedBookmarksErrorTitle: String = "Failed to fetch bookmarks"
 }
