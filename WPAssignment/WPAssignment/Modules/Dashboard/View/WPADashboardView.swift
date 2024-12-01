8607457d7b2184ac23048e7df505f63100e2164f
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-12-01 20:10:29 +1300

    Dashboard View refactoring

--- WPAssignment/WPAssignment/Modules/Dashboard/View/WPADashboardView.swift
+++ WPAssignment/WPAssignment/Modules/Dashboard/View/WPADashboardView.swift
@@ -9,16 +9,38 @@ import Foundation
 import SwiftUI
 
 struct WPADashboardView: View {
+    @StateObject private var viewModel = WPADashboardViewModel()
+    
     var body: some View {
         TabView {
+            contentView
+                .onAppear() {
+                    viewModel.fetchTabItems()
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private var contentView: some View {
+        if viewModel.tabItems.isEmpty {
+            WPAProgressView(message: WPAGenericConstants.kLoadingPlaceholderDescription)
+        } else {
+            ForEach(viewModel.tabItems, id: \.title) { tabItem in
+                tabView(for: tabItem)
+                    .tabItem {
+                        Label(tabItem.title, systemImage: tabItem.tabImageName)
+                    }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func tabView(for tabItem: TabItemData) -> some View {
+        switch tabItem.tabType {
+        case .creditCards:
             WPACreditCardsView()
-                .tabItem {
-                    Label(WPAGenericConstants.TabBar.kCreditCardTabTitle, systemImage: WPAGenericConstants.TabBar.kCreditCardImageName)
-                }
+        case .bookmarks:
             WPABookmarksView()
-                .tabItem {
-                    Label(WPAGenericConstants.TabBar.kBookmarksTabTitle, systemImage: WPAGenericConstants.TabBar.kBookmarkImageName)
-                }
         }
     }
 }
--- WPAssignment/WPAssignment/Modules/Dashboard/ViewModel/WPADashboardViewModel.swift
+++ WPAssignment/WPAssignment/Modules/Dashboard/ViewModel/WPADashboardViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum TabType: String, CaseIterable {
+    case creditCards
+    case bookmarks
+}
+
+struct TabItemData {
+    let title: String
+    let tabImageName: String
+    let tabType: TabType
+}
+
+class WPADashboardViewModel: ObservableObject {
+    @Published var tabItems: [TabItemData] = []
+    
+    func fetchTabItems() {
+        tabItems = [
+            TabItemData(
+                title: WPAGenericConstants.TabBar.kCreditCardTabTitle,
+                tabImageName: WPAGenericConstants.TabBar.kCreditCardImageName,
+                tabType: .creditCards),
+            TabItemData(
+                title: WPAGenericConstants.TabBar.kBookmarksTabTitle,
+                tabImageName: WPAGenericConstants.TabBar.kBookmarkImageName,
+                tabType: .bookmarks)
+        ]
+    }
+}
