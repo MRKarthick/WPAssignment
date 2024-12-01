095463badfd3d8706590631060f729e138fe8f13
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-12-02 02:47:25 +1300

    Comments for CardViewModel

--- WPAssignment/WPAssignment/Modules/Cards/ViewModel/WPACreditCardViewModel.swift
+++ WPAssignment/WPAssignment/Modules/Cards/ViewModel/WPACreditCardViewModel.swift
@@ -17,23 +17,27 @@ class WPACreditCardViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
     private let cardService: WPACreditCardServiceProtocol
     
+    // Initializer with dependency injection for card service
     init(cardService: WPACreditCardServiceProtocol = WPACreditCardService.shared) {
         self.cardService = cardService
     }
     
+    // Fetches cards, optionally forcing a refresh
     func fetchCards(isForceFetch: Bool = false) {
         cardService.fetchCards(isForceFetch: isForceFetch)
-            .receive(on: DispatchQueue.main)
+            .receive(on: DispatchQueue.main) // Ensure updates are on the main thread
             .sink(receiveCompletion: { [weak self] completion in
                 if case .failure(let error) = completion {
                     self?.errorMessage = "Failed to fetch cards: \(error.localizedDescription)"
                 }
             }, receiveValue: { [weak self] fetchedCards in
+                // Group fetched cards by type and sort them
                 self?.groupedCards = Dictionary(grouping: fetchedCards, by: { $0.ccType }).sorted(by: { $0.key < $1.key })
             })
             .store(in: &cancellables)
     }
     
+    // Toggles the bookmark status of a card
     func toggleBookmark(card: WPACreditCardDTO) {
         cardService.updateBookmark(forCardWithCcUid: card.ccUid, withValue: !card.isBookmarked)
     }
