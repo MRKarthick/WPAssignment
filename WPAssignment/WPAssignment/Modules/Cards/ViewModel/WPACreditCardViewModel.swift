be0a72b0b2a22a56f2920f3d3dea4102e98eb1d8
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-12-01 18:59:50 +1300

    Code cleanup

--- WPAssignment/WPAssignment/Modules/Cards/ViewModel/WPACreditCardViewModel.swift
+++ WPAssignment/WPAssignment/Modules/Cards/ViewModel/WPACreditCardViewModel.swift
@@ -12,14 +12,10 @@ class WPACreditCardViewModel: ObservableObject {
     @Published var groupedCards: [(key: String, value: [WPACreditCardDTO])] = []
     @Published var cardsDto: [WPACreditCardDTO] = []
     @Published var errorMessage: String? = nil
-    @Published var isLoading: Bool = false
     
     func fetchCards(isForceFetch: Bool = false) {
-        isLoading = true
-        
         WPACreditCardService.shared.fetchCards(completion: { [weak self] result in
             DispatchQueue.main.async {
-                self?.isLoading = false
                 switch result {
                 case .success(let fetchedCards):
                     self?.groupedCards = Dictionary(grouping: fetchedCards, by: { $0.ccType }).sorted(by: { $0.key < $1.key })
