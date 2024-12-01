4dba930a487cdbb032175b271a7fc0837b2d0788
Karthick Mannarkudi Ramesh Kumar <m.rkarthick900@gmail.com>
2024-12-02 03:35:06 +1300

    Removing unused files

--- WPAssignment/WPAssignmentUITests/Modules/WPABookmarksViewUITests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WPABookmarksViewUITests.swift
-//  WPAssignmentUITests
-//
-//  Created by EdgeCaseDesigns on 02/12/24.
-//
-
-import Foundation
--- WPAssignment/WPAssignmentUITests/Modules/WPACreditCardsViewUITests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WPACreditCardsViewUITests.swift
-//  WPAssignmentUITests
-//
-//  Created by EdgeCaseDesigns on 02/12/24.
-//
-
-import Foundation
